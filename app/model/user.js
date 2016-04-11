c03ca02d43c8fa53ce2f454d2758535b1d44afeb
jonassimonsen <jks.ufo@icloud.com>
2016-04-11 11:12:17 +0200

    Many different changes

--- app/model/user.js
+++ app/model/user.js
@@ -5,8 +5,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt');
 
-// Thanks to http://blog.matoski.com/articles/jwt-express-node-mongoose/
-
 // set up a mongoose model
 var UserSchema = new Schema({
     userName: {
